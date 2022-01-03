9b5b50540446ffea25f1d681be3dee979a4f624e
CashabackLab <97047873+CashabackLab@users.noreply.github.com>
2022-01-03 13:37:40 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings 
+    name='FileManagement',
+    url='https://github.com/CashabackLab/FileManagement',
+    author='CashabackLab',
+    author_email='cashabacklab@gmail.com',
+    # Needed to actually package something
+    packages=['statistics'],
+    # Needed for dependencies
+    install_requires=['numpy', 'os', 'pickle'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='UD',
+    description='Python package for managing file input/output and file saving, tailored for the Cashaback Lab',
+    long_description=open('README.txt').read(),
+)
