1aded4122e1fe23c70a7a7fe54597600953484f1
CashabackLab <97047873+CashabackLab@users.noreply.github.com>
2022-01-04 13:36:57 -0500

    Update varName.py

--- file_management/varName.py
+++ file_management/varName.py
@@ -2,6 +2,8 @@ def varName(p, name_space = globals()):
     """
     Return name of the variable as a string
     p: variable of any type
+    name_space: namespace of the variable to use
+    NOTE: must specify name_space = globals() when calling the function, should not be omitted
     """
     for k, v in name_space.items():
         if id(p) == id(v):
