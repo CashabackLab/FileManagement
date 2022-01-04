46fe7bb003e4cc164522eef205aeda22a43203ff
CashabackLab <97047873+CashabackLab@users.noreply.github.com>
2022-01-04 08:36:49 -0500

    Update __init__.py

--- file_management/__init__.py
+++ file_management/__init__.py
@@ -1,8 +1,2 @@
-def varName(p):
-    """
-    Return name of the variable as a string
-    p: variable of any type
-    """
-    for k, v in globals().items():
-        if id(p) == id(v):
-            return k
+from . import fileio
+from . import varName
