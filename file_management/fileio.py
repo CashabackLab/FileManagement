62e10119a6f4d283b16af21af8030b999ca5c81a
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-02-14 11:39:41 -0500

    Update fileio.py

--- file_management/fileio.py
+++ file_management/fileio.py
@@ -14,25 +14,33 @@ def get_most_recent_file(path, key_string = "loss"):
         most_recent_time = -1 #used if two files have the same date, will return most recent file by edit date
         most_recent_file = ""
         
+        key_string_flag = 0
+        date_flag = 0
         #for every file
         for filename in filenames:
             #check if key_string is in the filename and that the filename ends with the year
-            if key_string in filename and filename[-8:-4].isnumeric():
-                #temp dat/time variables
-                year  = filename[-8:-4]
-                month = filename[-11:-9]
-                day   = filename[-14:-12]
-                time   = os.path.getctime(os.path.join(path, filename))
-                
-                #Check if this file is more recent than the previous file
-                if int(year) >= int(most_recent_year):
-                    most_recent_year = year
-                    if int(month) >= int(most_recent_month):
-                        most_recent_month = month
-                        if int(day) >= int(most_recent_day):
-                            most_recent_day = day
-                            if time > most_recent_time:
-                                most_recent_time = time
-                                most_recent_file = filename
-        
+            if key_string in filename:
+                key_string_flag = 1
+                if filename[-8:-4].isnumeric():
+                    date_flag = 1
+                    #temp dat/time variables
+                    year  = filename[-8:-4]
+                    month = filename[-11:-9]
+                    day   = filename[-14:-12]
+                    time   = os.path.getctime(os.path.join(path, filename))
+
+                    #Check if this file is more recent than the previous file
+                    if int(year) >= int(most_recent_year):
+                        most_recent_year = year
+                        if int(month) >= int(most_recent_month):
+                            most_recent_month = month
+                            if int(day) >= int(most_recent_day):
+                                most_recent_day = day
+                                if time > most_recent_time:
+                                    most_recent_time = time
+                                    most_recent_file = filename
+                                    
+        if not key_string_flag: raise FileNotFoundError(f"No file with key_string "{key_string}" exists.")
+        if not date_flag: raise FileNotFoundError("No file with properly formatted date found.")
+          
         return most_recent_file
