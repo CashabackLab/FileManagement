b88371af28b2c35b651d148cb82165fdcb602ffc
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-04-15 10:39:45 -0400

    Update __init__.py

--- file_management/__init__.py
+++ file_management/__init__.py
@@ -2,6 +2,7 @@
 #i.e. file_management.varName()
 from .varName import *
 from .fileio import *
-from .nameof import nameof
+#from .nameof import nameof
+from varname import nameof
 from .copyfolderstruct import *
 from .reloadpackage import reloadpackage
