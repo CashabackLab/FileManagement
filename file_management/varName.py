795a5809773ab493632581dfaf75f4fadc58c51f
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-04-15 10:43:35 -0400

    Update varName.py

--- file_management/varName.py
+++ file_management/varName.py
@@ -1,10 +1,16 @@
-def varName(p, name_space = globals()):
+#This function is legacy code kept for compatability. U
+def varName(p, name_space = None):
     """
+    LEGACY CODE. Use file_management.nameof instead
+    
     Return name of the variable as a string
     p: variable of any type
     name_space: namespace of the variable to use
     NOTE: must specify name_space = globals() when calling the function, should not be omitted
     """
+    if name_space == None:
+        Raise ValueError("Must set name_space = globals()")
+        
     for k, v in name_space.items():
         if id(p) == id(v):
             return k
