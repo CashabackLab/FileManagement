0cad8bd3acdf3fe2487d598f4ed875e15dd9b08c
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-07-18 13:33:55 -0400

    Update __init__.py

--- file_management/__init__.py
+++ file_management/__init__.py
@@ -1,6 +1,6 @@
 #allows usage of the function from the package name directly
 #i.e. file_management.varName()
-import .Roth_FM
+from . import Roth_FM
 
 from .varName import *
 from .fileio import *
