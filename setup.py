1138cd0587d4d0dc434e173d2ad39da880446d1e
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-07-18 13:19:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Needed for dependencies
     install_requires=['numpy', 'varname'],
     # *strongly* suggested for sharing
-    version='0.3.4',
+    version='0.3.5',
     # The license can be anything you like
     license='MIT',
     description='Python package for managing file input/output and file saving, tailored for the Cashaback Lab',
