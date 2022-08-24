71d17e346ab65c853def97d3ad005d16324e92f9
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-08-24 09:46:09 -0400

    Create create_folder.py

---
+++ file_management/create_folder.py
@@ -0,0 +1,12 @@
+import os
+
+def create_folder(folder_name, path = ""):
+    """
+    Creates folder in current directory if it does not already exist. 
+    Can optionally set path to create folder in a different directory.
+    """
+    path = os.path.normpath(path)
+    full_path = os.path.join(path, folder_name)
+    
+    if not os.path.exists(full_path):
+        os.makedirs(full_path)
