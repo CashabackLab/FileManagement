eb7172b053e3114f0de8be4fa473cec11f127617
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-10-26 13:30:25 -0400

    Update __init__.py

--- file_management/__init__.py
+++ file_management/__init__.py
@@ -1,6 +1,6 @@
 #allows usage of the function from the package name directly
 #i.e. file_management.varName()
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 
 from .get_condition_data import * 
 from .get_trial_table import * 
@@ -8,9 +8,10 @@ from .get_target_table import *
 from .create_folder import create_folder
 
 from .varName import *
-from .fileio import *
+from .fileio import get_most_recent_file
 #from .nameof import nameof
 from varname import nameof, argname
 from .copyfolderstruct import *
 from importlib import reload
 
+from os.path import join, normpath
