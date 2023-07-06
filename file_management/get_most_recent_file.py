640aed369918510ddfb0e0a3752184d99a134e64
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2023-07-06 13:42:24 -0400

    overhauled function
    now accepts list of strings for key_string, swapped default dateformat, now more accurate across months

--- file_management/get_most_recent_file.py
+++ file_management/get_most_recent_file.py
@@ -1,48 +1,68 @@
 import os
 import os.path
 
-def get_most_recent_file(path, key_string = "loss", dateformat = "DD_MM_YYYY"):
-    """dateformat : {"MM_DD_YYYY", "DD_MM_YYYY"}"""
+def get_most_recent_file(path, key_string = "", dateformat = "MM_DD_YYYY"):
+    """
+    key_string can be a single string or a list of strings
+    dateformat : {"MM_DD_YYYY", "DD_MM_YYYY"}
+    assumes file is a .pkl"""
     if dateformat not in ["MM_DD_YYYY", "DD_MM_YYYY"]:
-        raise ValueError("Improper date format. set dateformat to either \"MM_DD_YYYY\" or \"DD_MM_YYYY\"")
-    for (dirpath, dirnames, filenames) in os.walk(path):
-        most_recent_year  = "0000"
-        most_recent_month = "0"
-        most_recent_day   = "0"
-        most_recent_time = -1 #used if two files have the same date, will return most recent file by edit date
-        most_recent_file = ""
+        raise ValueError("Improper date format. Set dateformat to either \"MM_DD_YYYY\" or \"DD_MM_YYYY\"")
         
+    for (dirpath, dirnames, filenames) in os.walk(path):
+        most_recent_file = ""
+        most_recent_date = {"MM": 0, "YYYY": 0, "DD": 0}
+        file_components = {"prefix": "", "date": "", "extension" : ""}
+       
         key_string_flag = 0
         date_flag = 0
+        file_found_flag = 0
         #for every file
         for filename in filenames:
             #check if key_string is in the filename and that the filename ends with the year
-            if key_string in filename:
-                key_string_flag = 1
-                if filename[-8:-4].isnumeric():
+            if isinstance(key_string, list):
+                for key in key_string:
+                    if key not in filename:
+                        key_string_flag = 0
+                        break
+                    key_string_flag = 1
+            else:
+                if key_string in filename:
+                    key_string_flag = 1
+                else:
+                    key_string_flag = 0
+                    
+            if key_string_flag:
+                
+                prefix, extension = os.path.splitext(filename)
+                file_components["prefix"] = prefix[:-10]
+                file_components["extension"] = extension
+                file_components["date"] = prefix[-10:]
+#                 print(file_components)
+                
+                if file_components["date"][-4:].isnumeric():
                     date_flag = 1
+                    file_found_flag = 1
                     #temp dat/time variables
-                    year  = filename[-8:-4]
-                    if dateformat == "MM_DD_YYY":
-                        day     = filename[-11:-9]
-                        month   = filename[-14:-12]
+                    year  = file_components["date"][-4:]
+                    if dateformat == "MM_DD_YYYY":
+                        day     = file_components["date"][3:5]
+                        month   = file_components["date"][0:2]
                     else:
-                        month = filename[-11:-9]
-                        day   = filename[-14:-12]
-                    time   = os.path.getctime(os.path.join(path, filename))
-
+                        month = file_components["date"][3:5]
+                        day   = file_components["date"][0:2]
+                       
                     #Check if this file is more recent than the previous file
-                    if int(year) >= int(most_recent_year):
-                        most_recent_year = year
-                        if int(month) >= int(most_recent_month):
-                            most_recent_month = month
-                            if int(day) >= int(most_recent_day):
-                                most_recent_day = day
-                                if time > most_recent_time:
-                                    most_recent_time = time
-                                    most_recent_file = filename
-                                    
-        if not key_string_flag: raise FileNotFoundError(f"No file with key_string \"{key_string}\" exists.")
-        if not date_flag: raise FileNotFoundError("No file with properly formatted date found.")
-          
+                    if int(year) >= int(most_recent_date["YYYY"]):
+                        most_recent_date["YYYY"] = year
+                        if int(month) >= int(most_recent_date["MM"]):
+                            most_recent_date["MM"] = month
+                            if int(day) >= int(most_recent_date["DD"]):
+                                most_recent_date["DD"] = day
+                                most_recent_file = "".join(file_components.values())
+
+                
+        if not key_string_flag and not file_found_flag: raise FileNotFoundError(f"No file with key_string \"{key_string}\" exists.")
+        if not date_flag and not file_found_flag: raise FileNotFoundError("No file with properly formatted date found.")
+         
         return most_recent_file
